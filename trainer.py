907ea8851a03fdc5053fe932173d3f3df23ae675
Valentino <valentino83@gmail.com>
2024-01-12 14:02:01 +0100

    Stravolto il codice per fare il tuning fine di imagenet

--- trainer.py
+++ trainer.py
@@ -2,6 +2,8 @@
 from tensorflow import keras
 from keras.utils import plot_model
 from imutils import paths
+import keras
+from keras.layers import Input, Conv2D, MaxPooling2D, Flatten, GRU, Dense, ReLU, GlobalAveragePooling2D,TimeDistributed
 
 from concurrent.futures import ThreadPoolExecutor
 
@@ -23,8 +25,7 @@ EPOCHS = 1500
 
 # MAX_SEQ_LENGTH = 5
 MAX_SEQ_LENGTH = 1500
-TRAIN_RANDOM_AUG = 5
-NUM_FEATURES = 2048
+TRAIN_RANDOM_AUG = 4
 
 MAX_THREAD_WORKERS = 12
 
@@ -67,52 +68,27 @@ def load_video(Temp, max_frames=0, resize=(R, C)):
             T = T[:, d1:d1+resize[1]]
 
 
-        frame = np.dstack((T[:,:], np.zeros(resize), np.zeros(resize)))
+        # frame = np.dstack((T[:,:], np.zeros(resize), np.zeros(resize)))
+        frame = np.dstack((T[:,:], T[:,:], T[:,:]))
 
         frames.append(frame)
 
     return np.array(frames)
 
-def build_feature_extractor():
-    feature_extractor = keras.applications.InceptionV3(
-        weights="imagenet",
-        include_top=False,
-        pooling="avg",
-        input_shape=(R, C, 3),
-    )
-    preprocess_input = keras.applications.inception_v3.preprocess_input
-
-    inputs = keras.Input((R, C, 3))
-    preprocessed = preprocess_input(inputs)
-
-    outputs = feature_extractor(preprocessed)
-    return keras.Model(inputs, outputs, name="feature_extractor")
-
-feature_extractor = build_feature_extractor()
-
-def prepare_video(fv, randomize, frame_features, frame_masks, nugDiams, nugDiam, CBDiams, CBDiam, videoRep, idx, reps):
+def prepare_video(fv, randomize, frame_features, nugDiams, nugDiam, CBDiams, CBDiam, videoRep, idx, reps):
     # Gather all its frames and add a batch dimension.
     if randomize:
         frames = load_video(fv.generateRandomData())
     else:
         frames = load_video(fv.saveTemp())
 
-    frames = frames[None, ...]
+    if frames.shape[0]>MAX_SEQ_LENGTH:
+        frames = frames[:MAX_SEQ_LENGTH, :]
 
-    # Initialize placeholders to store the masks and features of the current video.
-    temp_frame_mask = np.zeros(shape=(1, MAX_SEQ_LENGTH,), dtype="bool")
-    temp_frame_features = np.zeros(shape=(1, MAX_SEQ_LENGTH, NUM_FEATURES), dtype="float32")
-
-    # Extract features from the frames of the current video.
-    for i, batch in enumerate(frames):
-        video_length = batch.shape[0]
-        length = min(MAX_SEQ_LENGTH, video_length)
-        for j in range(length):
-            temp_frame_features[i, j, :] = feature_extractor.predict(batch[None, j, :])
-        temp_frame_mask[i, :length] = 1  # 1 = not masked, 0 = masked
-
-    frame_features[idx*reps+videoRep,] = temp_frame_features.squeeze()
-    frame_masks[idx*reps+videoRep,] = temp_frame_mask.squeeze()
+    if frames.shape[0]<MAX_SEQ_LENGTH:
+         frames = np.concatenate((frames, np.zeros((MAX_SEQ_LENGTH-frames.shape[0], R,C,3))),0)
+    
+    frame_features[idx*reps+videoRep,] = frames
 
     nugDiams[idx*reps+videoRep] = nugDiam
     CBDiams[idx*reps+videoRep] = CBDiam
@@ -124,12 +100,9 @@ def prepare_all_videos(df, randomize, reps):
 
     # diams = df["diam"].values
 
-    # `frame_masks` and `frame_features` are what we will feed to our sequence model.
-    # `frame_masks` will contain a bunch of booleans denoting if a timestep is
-    # masked with padding or not.
-    frame_masks = np.zeros(shape=(num_samples, MAX_SEQ_LENGTH), dtype="bool")
+    # `frame_features` are what we will feed to our sequence model.
     frame_features = np.zeros(
-        shape=(num_samples, MAX_SEQ_LENGTH, NUM_FEATURES), dtype="float32"
+        shape=(num_samples, MAX_SEQ_LENGTH, R, C, 3, ), dtype="float32"
     )
     nugDiams = np.zeros(shape=(num_samples,1), dtype="float32")
     CBDiams = np.zeros(shape=(num_samples,1), dtype="float32")
@@ -145,101 +118,98 @@ def prepare_all_videos(df, randomize, reps):
             fv[idx].videoCut(fv[idx].findExcitmentPeriod(200))
 
             for videoRep in range(reps):
-                executor.submit(prepare_video, fv[idx], randomize, frame_features, frame_masks, nugDiams, df["Nugget"].values[idx], CBDiams, df["CB"].values[idx], videoRep, idx, reps)
+                executor.submit(prepare_video, fv[idx], randomize, frame_features, nugDiams, df["Nugget"].values[idx], CBDiams, df["CB"].values[idx], videoRep, idx, reps)
+                # prepare_video(fv[idx], randomize, frame_features, nugDiams, df["Nugget"].values[idx], CBDiams, df["CB"].values[idx], videoRep, idx, reps)
 
     fv = []
-    return (frame_features, frame_masks), nugDiams, CBDiams
+    return frame_features, nugDiams, CBDiams
 
 train_data, train_nugDiams, train_CBDiams = prepare_all_videos(train_df, True, TRAIN_RANDOM_AUG)
+print("Alive")
 test_data, test_nugDiams, test_CBDiams = prepare_all_videos(test_df, False, 1)
 
-normNugDiam = test_nugDiams.max()
-train_nugDiams = train_nugDiams/normNugDiam
-test_nugDiams = test_nugDiams/normNugDiam
+maxNugDiam = test_nugDiams.max()
+minNugDiam = test_nugDiams.min()
+train_nugDiams = (train_nugDiams-minNugDiam)/(maxNugDiam-minNugDiam)
+test_nugDiams = (test_nugDiams-minNugDiam)/(maxNugDiam-minNugDiam)
 
-normCBDiam = test_CBDiams.max()
-train_CBDiams = train_CBDiams/normCBDiam
-test_CBDiams = test_CBDiams/normCBDiam
+maxCBDiam = test_CBDiams.max()
+minCBDiam = test_CBDiams.min()
+train_CBDiams = (train_CBDiams-minCBDiam)/(maxCBDiam-minCBDiam)
+test_CBDiams = (test_CBDiams-minCBDiam)/(maxCBDiam-minCBDiam)
 
-# print(f"Frame features in train set: {train_data[0].shape}")
-# print(f"Frame masks in train set: {train_data[1].shape}")
+print(f"Frame features in train set: {train_data.shape}")
 
 def get_sequence_model():
-    frame_features_input = keras.Input((MAX_SEQ_LENGTH, NUM_FEATURES))
-    mask_input = keras.Input((MAX_SEQ_LENGTH,), dtype="bool")
+    video_input = keras.Input(shape=(MAX_SEQ_LENGTH, R, C, 3))
 
-    # Refer to the following tutorial to understand the significance of using `mask`:
-    # https://keras.io/api/layers/recurrent_layers/gru/
-    # x = keras.layers.GRU(16, return_sequences=True)(
-    #     frame_features_input, mask=mask_input
-    # )
-    # x = keras.layers.GRU(8)(x)
-    # x = keras.layers.Dropout(0.6)(x)
-    # x = keras.layers.Dense(8, activation="relu")(x)
-    # output = keras.layers.Dense(1, activation="relu")(x)
+    imagenet_model= keras.applications.vgg16.VGG16(input_shape=(R,C,3),weights='imagenet', include_top=False)
 
+    imagenet_layers = imagenet_model.layers
+    for layer in imagenet_layers:
+        layer.trainable = False
+    imagenet = keras.Model(imagenet_model.input, imagenet_layers[-1].output)
 
-    # Questo mi piace
-    # x = keras.layers.GRU(24, return_sequences=True)(
-    #     frame_features_input, mask=mask_input
-    # )
-    # x = keras.layers.Dropout(0.5)(x)
-    # x = keras.layers.GRU(12)(x)
-    # x = keras.layers.Dropout(0.5)(x)
-    # x = keras.layers.Dense(8, activation="relu")(x)
-    # x = keras.layers.Dropout(0.5)(x)
+    x = TimeDistributed(imagenet)(video_input)
+    x = TimeDistributed(GlobalAveragePooling2D())(x)
 
-    # x = keras.layers.GRU(32, return_sequences=True)(
-    #     frame_features_input, mask=mask_input
-    # )
-
-    x = keras.layers.GRU(64, return_sequences=True)(
-        frame_features_input)
+    x = GRU(128, activation='relu', return_sequences = True)(x)
     x = keras.layers.Dropout(0.4)(x)
-    x = keras.layers.GRU(32)(x)
+    x = GRU(64, activation='relu', return_sequences = False)(x)
     x = keras.layers.Dropout(0.2)(x)
-    x = keras.layers.Dense(16, activation="relu")(x)
+    x = Dense(64, activation='relu')(x)
     x = keras.layers.Dropout(0.2)(x)
-    x = keras.layers.Dense(8, activation="relu")(x)
+    x = Dense(32, activation='relu')(x)
+    x = Dense(16, activation='relu')(x)
 
     out1 = keras.layers.Dense(1, activation="linear", name='Nugget')(x)
     out2 = keras.layers.Dense(1, activation="linear", name='CB')(x)
 
-    rnn_model = keras.Model(frame_features_input, [out1, out2])
+    rnn_model = keras.Model(video_input, out1)
 
-    rnn_model.compile(
-        loss="mean_absolute_error", optimizer="adam", metrics=["mae", "mse"]
-    )
+    rnn_model.compile(loss='mse', optimizer='adam', metrics=['mae'])
     return rnn_model
 
-def plot_loss(history):
+def plot_loss(history, fileName):
     plt.plot(history.history['loss'], label='loss')
     plt.plot(history.history['val_loss'], label='val_loss')
     plt.ylim([0, 10])
     plt.xlabel('Epoch')
-    plt.ylabel('Error')
+    plt.ylabel('Loss')
     plt.legend()
     plt.grid(True)
-    plt.savefig('history.pdf')
+    plt.savefig(fileName)
+    plt.close()
 
 def run_experiment():
+    #  Early stopping
+    early_stopping = keras.callbacks.EarlyStopping(monitor='val_loss', patience=100)
+
     filepath = "./video_classifier"
     checkpoint = keras.callbacks.ModelCheckpoint(
         filepath, save_weights_only=True, save_best_only=True, verbose=1
     )
 
     seq_model = get_sequence_model()
+
     # print(seq_model.summary())
-    # plot_model(seq_model, to_file='model.png', show_shapes=True, show_layer_names=True)
+    plot_model(seq_model, to_file='model.png', show_shapes=True, show_layer_names=True)
     history = seq_model.fit(
-        train_data[0],
-        [train_nugDiams, train_CBDiams],
+        train_data,
+        train_nugDiams,
         epochs=EPOCHS,
-        callbacks=[checkpoint],
-        validation_data=(test_data[0], [test_nugDiams, test_CBDiams])
+        callbacks=[checkpoint, early_stopping],
+        validation_data=(test_data, test_nugDiams)
     )
 
-    plot_loss(history)
+    print('First Training completed')
+
+    #print metrics
+    # Print loss and validation loss
+    print('Loss:', history.history['loss'])
+    print('Validation loss:', history.history['val_loss'])
+
+    plot_loss(history, 'history1.pdf')
     seq_model.save('model.h5')
 
     # history = []
@@ -248,34 +218,34 @@ def run_experiment():
     # _, mse = seq_model.evaluate(test_data[0], [test_nugDiams, test_CBDiams])
     # print(f"Validation MAE {mse}")
 
+    for layer in seq_model.layers:
+        layer.trainable = True
+
+    seq_model.compile(optimizer=keras.optimizers.Adam(lr=0.0001), loss='mse')  # Lower learning rate
+
+    history = seq_model.fit(
+        train_data,
+        train_nugDiams,
+        epochs=EPOCHS,
+        callbacks=[checkpoint, early_stopping],
+        validation_data=(test_data, test_nugDiams)
+    )
+
+    print("Second training completed")
+    plot_loss(history, 'history2.pdf')
+    seq_model.save('modelDef.h5')
+    seq_model.load_weights(filepath)
+
     return history, seq_model
 
 
 _, sequence_model = run_experiment()
 
-def prepare_single_video(frames):
-    frames = frames[None, ...]
-    frame_mask = np.zeros(shape=(1, MAX_SEQ_LENGTH,), dtype="bool")
-    frame_features = np.zeros(shape=(1, MAX_SEQ_LENGTH, NUM_FEATURES), dtype="float32")
-
-    for i, batch in enumerate(frames):
-        video_length = batch.shape[0]
-        length = min(MAX_SEQ_LENGTH, video_length)
-        for j in range(length):
-            frame_features[i, j, :] = feature_extractor.predict(batch[None, j, :])
-        frame_mask[i, :length] = 1  # 1 = not masked, 0 = masked
-
-    return frame_features, frame_mask
-
-
-def sequence_prediction(path):
-    fv = FlirVideo(path)
-    fv.videoCut(fv.findExcitmentPeriod(200))
-    frames = load_video(fv.saveTemp())
-    frame_features, frame_mask = prepare_single_video(frames)
-    diams = sequence_model.predict([frame_features])
+def sequence_prediction(idx):
+    video = test_data[idx,:]
+    diam = sequence_model.predict(video)
     # print(f"Estimated diams: {diams[0]}, {diams[1]}")
-    return diams[0], diams[1]
+    return diam
 
 est_nugDiam = np.zeros(len(test_df))
 est_CBDiam = np.zeros(len(test_df))
@@ -287,16 +257,16 @@ with open('out.csv', 'w', encoding='UTF8') as fd:
 
     for idx, path in enumerate(test_df["fileName"].values.tolist()):
         # print(f"Test video path: {path}")
-        est_nugDiam[idx], est_CBDiam[idx] = sequence_prediction(path)
+        est_nugDiam[idx] = sequence_prediction(idx)
 
-        writer.writerow([path, nugDiams[idx], CBDiams[idx], est_nugDiam[idx]*normNugDiam, est_CBDiam[idx]*normCBDiam])
+        writer.writerow([path, nugDiams[idx], CBDiams[idx], est_nugDiam[idx]*(maxNugDiam-minNugDiam)+minNugDiam])
 
 plt.clf()
-plt.plot(nugDiams, est_nugDiam*normNugDiam, 'r+')
+plt.plot(nugDiams, est_nugDiam*(maxNugDiam-minNugDiam)+minNugDiam, 'r+')
 plt.plot(nugDiams, nugDiams)
 plt.savefig('validation_nugget.pdf')
 
-plt.clf()
-plt.plot(CBDiams, est_CBDiam*normCBDiam, 'r+')
-plt.plot(CBDiams, CBDiams)
-plt.savefig('validation_CB.pdf')
+# plt.clf()
+# plt.plot(CBDiams, est_CBDiam*normCBDiam, 'r+')
+# plt.plot(CBDiams, CBDiams)
+# plt.savefig('validation_CB.pdf')
